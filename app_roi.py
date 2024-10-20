5a397d5085c1799f16fc9460524c987f55e053d1
CassioSperb <cassiosperb@gmail.com>
2024-10-20 10:04:59 -0500

    creating flask, not loading database yet

---
+++ app_roi.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas as pd
+
+# Initialize Flask app, setting the template folder to the root directory
+app = Flask(__name__)
+
+# Load the datasets
+df_roi = pd.read_csv('../data/outputs/cumulative_roi_results.csv')
+
+# Define the main route to render index.html from the root directory
+@app.route('/')
+def index():
+    return render_template('roi_chart.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
